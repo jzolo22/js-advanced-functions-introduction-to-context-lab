e3891de8d9419d8a2a58ed995b47d167512c79a0
jzolo22 <jzolotarev@gmail.com>
2020-11-29 18:37:41 -0500

    commit tree 7796ced2bcead8535d01de5c31a76c1022e6eaa6

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Your code here
+function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
+    return {
+        firstName,
+        familyName,
+        title,
+        payPerHour,   
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords (employeeArray) {
+    const newArray = []
+    employeeArray.forEach(employee => {
+        newArray.push(employee.createEmployeeRecord)
+    })
+    return newArray
+}
